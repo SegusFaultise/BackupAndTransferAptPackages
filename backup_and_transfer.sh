0d8c1932159db3736ce848744447d0415d6121d2
zakk wilson-christian <115408369+NeuralNuts@users.noreply.github.com>
2024-01-20 13:26:12 +1000

    Update backup_and_transfer.sh

--- backup_and_transfer.sh
+++ backup_and_transfer.sh
@@ -6,7 +6,7 @@ dpkg --get-selections > package_list.txt
 
 echo "#!/bin/bash" > "$install_script"
 echo "" >> "$install_script"
-echo "# Install all packages listed in $package_list_file" >> "$install_script"
+echo "# Install all packages listed in package_list.txt" >> "$install_script"
 echo "while read package _status; do" >> "$install_script"
 echo "    sudo apt-get install -y \$package" >> "$install_script"
 echo "done < package_list.txt" >> "$install_script"
@@ -14,4 +14,4 @@ echo "done < package_list.txt" >> "$install_script"
 chmod +x "$install_script"
 
 echo "Package list saved to package_list.txt"
-echo "To install these packages on another computer, transfer $package_list_file and $install_script to that computer, and then run ./$install_script"
+echo "To install these packages on another computer, transfer package_list.txt and $install_script to that computer, and then run ./$install_script"
